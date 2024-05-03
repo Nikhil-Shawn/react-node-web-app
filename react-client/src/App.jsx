d9bc142b463dbd6a320243501668e2aad1e70f60
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-05-04 00:39:31 +0200

    Stock check added

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -4,9 +4,29 @@ import Login from './pages/Login';
 import ProductPage from './pages/ProductPage';
 import Register from './pages/Register';
 import SingleProductPage from './pages/SingleProductPage';
+import {BrowserRouter as Router, Route, Link,
+Routes} from "react-router-dom"
+
 
 const App = () => {
-  return  <Cart/>
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/products" element={<ProductPage />}>
+        </Route>
+        <Route path="/login" element={<Login />}>
+        </Route>
+        <Route path="/register" element={<Register />}>
+        </Route>
+        <Route path="/product:id" element={<SingleProductPage />}>
+        </Route>
+        <Route path="/cart" element={<Cart />}>
+        </Route>
+        </Routes>
+    </Router>
+  )
 };
 
 export default App;
