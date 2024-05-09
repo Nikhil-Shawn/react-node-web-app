4c4a323e4585c17070bee00a0d00d4af927dab4e
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-05-09 19:08:28 +0200

    updated cors

--- node-express-api/index.js
+++ node-express-api/index.js
@@ -8,8 +8,16 @@ const notFound = require('./middleware/not-found')
 const cart = require('./routes/cart')
 const order = require('./routes/order')
 
+const cors=require("cors");
 dotenv.config();
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json())
 app.use('/api/v1/', user)
 app.use('/api/v1/product', product)
