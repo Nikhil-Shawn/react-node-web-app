aa5522fd5535ced1a8131e712cfa39c10a994baa
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-07-08 23:12:22 +0200

    Improved success page UI

--- react-client/src/pages/Success.jsx
+++ react-client/src/pages/Success.jsx
@@ -1,29 +1,67 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useLocation } from "react-router";
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
 
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const Message = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: teal;
+  color: white;
+  border: none;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: darkcyan;
+  }
+`;
 
 const Success = () => {
   const location = useLocation();
-  const data = location.state.stripeData;
-  const cart = location.state.cart;
-  const [orderId, setOrderId] = useState(null);
+  const navigate = useNavigate();
+
+  const handleContinueShopping = () => {
+    navigate('/');
+  };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {orderId
-        ? `Order has been created successfully. Your order number is ${orderId}`
-        : `Successfull. Your order is being prepared...`}
-      <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
-    </div>
+    <Container>
+      <Navbar />
+      <Announcement />
+      <MainContent>
+        <Message>
+          {location.state && location.state.data
+            ? `Payment was successful! Your order ID is ${location.state.data.id}`
+            : 'Payment was successful!'}
+        </Message>
+        <Button onClick={handleContinueShopping}>Continue Shopping</Button>
+      </MainContent>
+      <Footer />
+    </Container>
   );
 };
 
