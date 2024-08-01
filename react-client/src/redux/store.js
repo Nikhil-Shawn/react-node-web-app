0ac85f00045f87df30777f05a98e76043bec5718
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-08-01 23:44:14 +0200

    redux-presist initialization

--- react-client/src/redux/store.js
+++ react-client/src/redux/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
 import cartReducer from './cartSlice';
-import userReducer from './userSlice'
+import userReducer from './userSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
