a810e6358cc0124f2f543a244983798058e2955c
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-08-25 21:11:37 +0200

    Added dynamic token and auth

--- admin-panel/src/requestmethod.js
+++ admin-panel/src/requestmethod.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/v1";
-const TOKEN = "";  // Replace with the actual token when available
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;  // Replace with the actual token when available
+
+console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
 
 // Public requests do not require authentication
 export const publicRequest = axios.create({
--- react-client/src/requestmethod.js
+++ react-client/src/requestmethod.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/v1";
-const TOKEN = "";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;  // Replace with the actual token when available
+
+console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
